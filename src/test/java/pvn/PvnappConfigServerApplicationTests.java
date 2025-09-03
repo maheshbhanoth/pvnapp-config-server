479dd3a3f5c0cf7cacffc131c66babdf65c1a5dd
maheshbhanoth <maheshbanoth762.com>
2025-09-04 00:15:30 +0530

    Initial commit - added config server project

---
+++ src/test/java/pvn/PvnappConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pvn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PvnappConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
